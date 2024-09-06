a2d1c9aeade28e17fc60d001786005d75fbf8a91
felipepassagem <64990930+felipepassagem@users.noreply.github.com>
2024-09-06 20:23:29 -0300

    .

--- backend/djoserauthapi/settings.py
+++ backend/djoserauthapi/settings.py
@@ -102,11 +102,11 @@ WSGI_APPLICATION = 'djoserauthapi.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'postgres',
-       'USER': 'postgres',
-       'PASSWORD': '120558',
-       'HOST': 'localhost',
-       'PORT': '5433',
+       'NAME':  os.environ.get('DB_NAME'),#'postgres',
+       'USER':  os.environ.get('DB_USER'),#'postgres',
+       'PASSWORD':  os.environ.get('DB_PASSWORD'),#'120558',
+       'HOST':  os.environ.get('DB_HOST'),#'localhost',
+       'PORT':  os.environ.get('DB_PORT'),#'5433',
    }
 }
 
